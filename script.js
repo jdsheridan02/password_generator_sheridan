6cc94f2faeba99363016613ee99ab41799cb4f25
jdsheridan02 <jdsheridan02@gmail.com>
2020-05-09 20:21:19 -0400

    completed hw

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// variables to included all needed characters and to ensure each caracter is included in the password
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "1234567890";
+var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var allCharacters = ""
+var neededCharacters = ""
+var finalpass = ""
+
+// function to generate password dependent on length and other attributes
+function generatePassword(){
 
 // prompt for password length
 var getLength = prompt("How many characters would you like your password to be (8-128)?")
 
-
 console.log(getLength)
-
-// variables to included all needed characters and to ensure each caracter is included in the password
-var allCharacters = []
-var neededCharacters = []
 
 // confirm statement for lower case characters
 var lower = confirm("Would you like to include lower case characters in your password?")
 
 if (lower === true) {
-  lower = "abcdefghijklmnopqrstuvwxyz";
+  
+  checkLower()
 }
 console.log(lower);
 
@@ -23,76 +28,77 @@ console.log(lower);
 var upper = confirm("Would you like to include upper case characters in your password?")
 
 if (upper === true) {
-  upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  checkUpper()
 }
 console.log(upper);
 
 // confirm statement for numberic characters
 var number = confirm("Would you like to include numeric characters in your password?")
 if (number === true) {
-  number = "1234567890";
+  checkNumber()
 }
 console.log(number);
 
 // confirm statment for special characters
 var special = confirm("Would you like to include special characters in your password?")
 if (special === true) {
-  special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  checkSpecial()
 }                                                              
 console.log(special);
 
 // functions to randomly choose a characters from each prompt
 function checkLower() {
-  return lower.charAt(Math.floor(Math.random() * lower.length));
+  var randomLower = lowerChar.charAt(Math.floor(Math.random()*lowerChar.length))
+  console.log(randomLower)
+  neededCharacters += randomLower
+  allCharacters += lowerChar
 }
-console.log(checkLower);
-  
-  neededCharacters.push()
-  allCharacters = allCharacters.concat()
-}
-function checkUpper() {
-  var upper = upper.chartAt(Math.floor(Math.random() * upper.length))
 
-  neededCharacters.push()
-  allCharacters = allCharacters.concat()
+function checkUpper() {
+  var randomUpper = upperChar.charAt(Math.floor(Math.random() * upperChar.length))
+  console.log(randomUpper)
+  neededCharacters += randomUpper
+  allCharacters += upperChar
 }
 function checkNumber(){
-  var number = number.chartAt(Math.floor(Math.random() * number.length))
-
-  neededCharacters.push()
-  allCharacters = allCharacters.concat()
-}
+  var randomNumber= numberChar.charAt(Math.floor(Math.random() * numberChar.length))
+  console.log(randomNumber)
+  neededCharacters += randomNumber
+  allCharacters += numberChar
+ }
 function checkSpecial(){
-  var special = special.chartAt(Math.floor(Math.random() * special.length))
+  var randomspecial = specialChar.charAt(Math.floor(Math.random() * specialChar.length))
+  console.log(randomspecial)
+  neededCharacters += randomspecial
+  allCharacters += specialChar
+}
+console.log(neededCharacters);
+console.log(allCharacters);
 
-  neededCharacters.push()
-  allCharacters = allCharacters.concat()
+// loop to include confirmed characters for the length required for password
+for (var t = 0; t < getLength; t++) {
+  finalpass += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+}
+console.log(neededCharacters)
+
+// loop to determine which characters are needed dependent on bulean responses
+for (var j=0; j<neededCharacters.length;j++){
+  finalpass[j]=neededCharacters[j]
 }
 
+console.log(finalpass)
 
-// Write password to the #password input
+// inputs final pass that meets the criteria into the text box of HTML
+return finalpass
+}
+
+  // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  console.log
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-  //loop to determine the necessary number of character and pull character from each array/string
-for (var i=0; i<getLength.length;i++) {
-var character = allCharacters[Math.floor(Math.random() * allCharacters.length)];
-
-console.log(character);
-
-// final.push(character);
 }
-
-for (var j=0; j<neededCharacters.length;j++){
-
-  final[j] = neededCharacters[j]
-}
-
-// return final.join("")
-}
-
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
